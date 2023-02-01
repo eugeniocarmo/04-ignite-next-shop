52ec0470a9eb3979ffd8412b04ed7c324ab78a19
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-02-01 21:08:15 +0000

    feat: "STD Home, Product, Success pages created"

---
+++ src/pages/product.tsx
@@ -0,0 +1,11 @@
+
+export default function Product () {
+  return (
+    <>
+      <h1>Product</h1>
+      <p>Welcome to the product page</p>
+    </>
+
+  )
+
+}
---
+++ src/pages/success.tsx
@@ -0,0 +1,11 @@
+
+export default function Success() {
+
+  return (
+    <>
+      <h1>Success</h1>
+      <p>You product purchsed successfully!</p>
+    </>
+  )
+
+};
