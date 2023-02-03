dcf89052ec15fde9fb313f4c2d601d5915473ca2
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-02-03 23:37:41 +0000

    feat: "Create global styles"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
+
 
 export default function App({ Component, pageProps }: AppProps) {
+  globalStyles();
+
   return <Component {...pageProps} />
 }
+
+
